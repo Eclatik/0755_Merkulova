08946f1b7a5f8ab9ca48467408586f310f93f434
Elizaveta Merkulova <111577120+Eclatik@users.noreply.github.com>
2022-09-05 14:20:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,34 +1,33 @@
 // ==UserScript==
-// @name         GoogleBot
+// @name         Open_HomeWork
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Bot for Google
+// @description  try to open the napli.ru
 // @author       Elizaveta Merkulova
-// @match        https://www.google.com/
+// @match        https://www.bing.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
 let links = document.links;
-let btnk = document.getElementByName("btnk")[0];
-let keywords = ["вывод произвольных полей wordpress", "10 самых популярных шрифтов от Google", "Отключение редаций и ревизий в WordPress"];
-let keyword = keywords[getRandom (0, keywords.lenght)];
+let search = document.getElementsByName("search")[0];
+let keywords = ["Взаимодействие PHP и MySQL", "10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress"];
+let keyword = keywords[getRandom(0, keywords.length)];
 
-
-if (btnk !== underfined) {
-  document.getElementByName ("q")[0].value = keywords;
-btnk.click();
+if (search !== undefined) {
+  document.getElementsByName("q")[0].value = keyword;
+  search.click();
 } else {
 
-for (let i=0; i<length; i++) {
-  if (links[i].href.indexOf("napli.ru")!== -1) {
-      console.log("Нашел строку" +links[i]);
-  let link = links[i];
-  link.click();
-  break;
-}
-}
+  for (let i=0; i<links.length; i++) {
+    if (links[i].href.indexOf("napli.ru") !== -1) {
+      console.log("Нашел строку " +links[i]);
+      let link = links[i];
+      link.click();
+      break;
+    }
+  }
 }
 function getRandom (min, max) {
-return Math.floor(Math.rendom()*(max-min)+min);
+  return Math.floor(Math.random()*(max-min)+min);
 }
